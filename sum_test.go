c3b1ed521d5939515544b7b0028ce8fa6cc77f27
Ardi Ismail <>
2019-09-14 00:48:17 +0700

    adding checkSums

--- sum_test.go
+++ sum_test.go
@@ -22,6 +22,7 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
+
 	t.Run("test 2 slice return 1 slice", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
@@ -33,22 +34,24 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTail(t *testing.T) {
-	t.Run("sum all members without head", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
-
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("sum all members without head", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
 
