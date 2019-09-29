d15f87b068339da4963a83a9fdde83bda232ec0c
Ardi Ismail <>
2019-09-29 14:17:54 +0700

    add sum func to run the test

--- sum.go
+++ sum.go
@@ -2,5 +2,9 @@ package arraynslice
 
 // Sum only return total sum of array value
 func Sum(numbers [5]int) int {
-	return 0
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
 }
